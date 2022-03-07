ef52bbb307953f6caaf5dadeb4394b978cd25f47
Cassandre <cassandre.crescence@etu.imt-lille-douai.fr>
2022-03-07 21:04:04 +0100

    quizz form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from "react"
 import Start from "./components/Start.js"
+import Quizz from "./components/Quizz.js"
 
 export default function App(){
 
+    const [startMenu, setStartMenu] = React.useState(true)
 
+    function startQuizz(){
+        setStartMenu(!startMenu)
+    }
 
+    const dataQuizz = fetch("https://opentdb.com/api.php?amount=5")
+        .then(res => res.json())
+        .then(quest => console.table(quest.results))
+  
+    const quizz = dataQuizz.map( data => {
+       return <Quizz question={data.question} />
+    })
+
+    console.log(quizz)
+    
     return(
     <div>
-        <Start />
-        
+        {startMenu && <Start startQuizz={startQuizz}/>}
     </div>
     )
 }
+
+//{!startMenu && quizz}
