bf02eb3670da70dbe1154d6458b1019b2235812d
Cassandre <cassandre.crescence@etu.imt-lille-douai.fr>
2022-03-07 18:28:38 +0100

    start interface

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,12 +1,12 @@
 import React from "react"
 
-export default function Start(){
+export default function Start(props){
 
     return(
         <div className="start--menu">
             <h1>Trivia game</h1>
-            <p>This game is just a simple quizz. If you are ready, click on the button below.</p>
-            <button>Start quizz</button>
+            <p className="start--description">This game is just a simple quizz. If you are ready, click on the button below.</p>
+            <button onClick={props.startQuizz}><h3>Start quizz</h3></button>
         </div>
     )
 }
