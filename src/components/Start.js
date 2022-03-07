464e3f015a9565260534762b429fbb9e25028ecd
Cassandre <cassandre.crescence@etu.imt-lille-douai.fr>
2022-03-07 16:34:26 +0100

    creating the start menu

---
+++ src/components/Start.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Start(){
+
+    return(
+        <div className="start--menu">
+            <h1>Trivia game</h1>
+            <p>This game is just a simple quizz. If you are ready, click on the button below.</p>
+            <button>Start quizz</button>
+        </div>
+    )
+}
