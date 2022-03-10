47f268a1a6a9094f9e2246cb2dd9e728ec78d599
Cassandre <cassandre.crescence@etu.imt-lille-douai.fr>
2022-03-10 12:01:10 +0100

    score function and working on how to play a new game

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import Start from "./components/Start.js"
 import Quizz from "./components/Quizz.js"
 import {nanoid} from "nanoid"
+import Confetti from 'react-confetti'
 
 export default function App(){
 
@@ -10,55 +11,121 @@ export default function App(){
     const [rawData, setRawData] = React.useState([])
     const [data, setData] = React.useState([])
     const [selection, setSelection] = React.useState([])
+    const [selectMode, setSelectMode] = React.useState(true)
+    const [score, setScore] = React.useState(0)
 
     function startQuizz(){
         setStartMenu(!startMenu)
+        setStartGame(prevGame => !prevGame)
     }
 
     React.useEffect( () => {
-        fetch("https://opentdb.com/api.php?amount=1&encode=base64")
+        fetch("https://opentdb.com/api.php?amount=3&encode=base64")
             .then(res => res.json())
             .then(quest => setRawData(quest.results))
         
         setData(() =>  rawData.map(item => ({
             ...item,
+            correct_answer: window.atob(item.correct_answer),
+            correct_answer_id: nanoid(),
             id: nanoid(),
             allAnswers: [].concat(item.incorrect_answers.map(element => ({
                 answer: window.atob(element),
                 id: nanoid(),
-            }))).concat({answer: window.atob(item.correct_answer), id: nanoid()}),
+            }))),
         })))
-    }, [startMenu])
 
-    
+        setData(prevData => prevData.map(item => ({
+            ...item,
+            allAnswers: item.allAnswers.concat({answer: item.correct_answer, id: item.correct_answer_id})
+        })))
+        
+        setData(prevData => prevData.map(item => ({
+            ...item,
+            allAnswers: item.allAnswers.sort(function(a,b){return Math.random() - 0.5})
+        })))
+        
+    }, [startGame])
+
+    React.useEffect(() =>{
+        getScore()
+    },[selectMode])
+
     console.log(data)
 
-    function selectAnswer(id){
-        setSelection(prevSelection => (id in prevSelection)?
-            prevSelection.splice(prevSelection.indexOf(id, 1)) :
-            prevSelection.concat(id)      
-        )
+    function selectedAnswer(id, questionId){
+
+        if(selectMode){
+            for(const element of data){
+                if(element.id === questionId){
+                    const allAnswers = element.allAnswers
+
+                for(const item of allAnswers){
+                    if(selection.indexOf(item.id) in selection && !(selection.indexOf(id) in selection)){
+                            setSelection(prevSelection => prevSelection.filter(sel => sel != item.id))
+                            setSelection(prevSelection => prevSelection.concat(id))
+                    }
+                } 
+                if(selection.indexOf(id) in selection){
+                        setSelection(prevSelection => prevSelection.filter(sel => sel != id))
+                    }
+                    else{
+                        setSelection(prevSelection => prevSelection.concat(id))
+                    } 
+                }
+            }}         
     }
-    
-    
+
     const quizz = data.map(item => {
             return <Quizz 
-                key={item.id} 
+                key={item.id}
+                id = {item.id} 
                 question={window.atob(item.question)}
-                correctAnswer={window.atob(item.correct_answer)}
+                correctAnswer={item.correct_answer}
                 answers={item.allAnswers}
-                selectAnswer={selectAnswer}
+                selectedAnswer={selectedAnswer}
                 selection={selection}
+                selectMode={selectMode}
             />
         })
+    
+    function clickCheckAnswers(){
+        setSelectMode(false)
+    }
 
+    function getScore(){
+        for(const element of selection){
+            for(const item of data){
+                if(element === item.correct_answer_id){
+                    setScore(prevScore => prevScore + 1)
+                }
+            }
+        }
+        return score
+    }
+
+    function playAgain(){
+        setRawData([])
+        setData([])
+        setSelection([])
+        setScore(0)
+        setSelectMode(true)
+        setStartGame(prevGame => !prevGame)
+    }
+    console.log(data)    
     return(
     <div>
-        {startMenu && <Start startQuizz={startQuizz}/>} 
+        {startMenu && <Start startQuizz={startQuizz}/>}
+        {!selectMode && <Confetti />}
         {!startMenu && 
             <div className="quizz--font">
                 {quizz}
-                <button className="quizz--button"><h3>Check answers</h3></button>
+                { selectMode && <button className="quizz--button" onClick={clickCheckAnswers}><h3>Check answers</h3></button>}
+                { !selectMode && 
+                    <div className="footer">
+                    <button className="quizz--button" onClick={playAgain} ><h3>Play again</h3></button>
+                    <p>Your score: {score}/3</p></div>
+                }
             </div>
         }
     </div>
--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -2,24 +2,24 @@ import React from "react"
 
 export default function Quizz(props){
 
-    let shuffledAnswerList = props.answers.sort(function(a,b){return Math.random() - 0.5})
-
-    console.log(shuffledAnswerList)
-    /*.map(value => ({ value, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value)*/
-
     const answers = props.answers.map(item => {
         return <div 
-            className={item.id in props.selection? "selected--answer":"answer" }
-            onClick={() => props.selectAnswer(item.id)}
+            className= {((props.selectMode) && (props.selection.indexOf(item.id) in props.selection))? "selected--answer": 
+                (props.selectMode)? "answer":
+                (!props.selectMode && item.answer === props.correctAnswer) ? "good--answer": 
+                (!props.selectMode && item.answer != props.correctAnswer && props.selection.indexOf(item.id) in props.selection ) ? "bad--answer":
+                "answer"
+            }
+            onClick={() => {
+                props.selectedAnswer(item.id, props.id);
+            }}
             > {item.answer}</div>
     })
 
-    //console.log(answers)
 
     return(
         <div className="quizz-question">
+            
             <h4>{props.question}</h4>
             <div className="answer--list">{answers}</div>
             <hr className="delimiter"/>
